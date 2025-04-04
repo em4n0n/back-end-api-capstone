96372946e5663d0e4277136ff4223490a4f99bdb
david <dmsim159@gmail.com>
2025-04-03 21:35:09 -0700

    replace userserializer with booking

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,13 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework import generics
 from .models import MenuItem
-from .serializers import UserSerializer
+from .serializers import BookingSerializer, Booking
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
 
 # Create your views here.
 class MenuItemView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = BookingSerializer
     
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = BookingSerializer
+    
+class BookingViewset(ModelViewSet):
+    queryset= Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
