6764760438895f5f30b490991d3a313d5059e4b5
david <dmsim159@gmail.com>
2025-04-03 21:34:52 -0700

    remove "'" from router

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -19,7 +19,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from restaurant import views
 
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewset)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/menu/', include('restaurant.urls')),
+    path('restaurant/booking/', include(router.urls)),
 ]
+
+
