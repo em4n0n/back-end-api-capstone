2c534a995e1d191c7fb8037334014332ecb0cf94
david <dmsim159@gmail.com>
2025-04-16 13:00:27 -0700

    move tests to project folder

--- littlelemon/littlelemon/tests/test_models.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from restaurant.models import MenuItem
-
-class MenuItemTest(TestCase):
-    def test_get_item(self):
-        item = MenuItem.objects.create(title="IceCream", price=10, inventory=100)
-        self.assertEqual(item.title, "IceCream")
-        self.assertEqual(item.price, 10.0)
-        self.assertEqual(item.inventory, 100)
